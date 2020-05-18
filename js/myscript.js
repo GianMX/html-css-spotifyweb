34fb0e6fce8456e1a7fc52ae2737bdb2edc582d6
Gianpiero <gpi91mx@gmail.com>
2020-05-18 17:15:16 +0200

    JS migliorato

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,4 @@
-document.getElementById('scrb') .innerHTML= 'Ascoltati di Recente'
+document.getElementById('scrb').onclick = function changeContent() {
+   document.getElementById('scrb').innerHTML = "Ascoltati di Recente";
+   document.getElementById('scrb').style = "font-style: italic ; color: purple";
+}
